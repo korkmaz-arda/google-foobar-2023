83c57570051c055dc54f1fb1e08b0cf93280a807
korkmaz-arda <arda.korkmaz1996@gmail.com>
2023-10-11 11:53:59 +0300

    add find-the-access-codes

---
+++ level3/find-the-access-codes/solution.py
@@ -0,0 +1,18 @@
+def solution(l):
+    divisors = {}
+    for i, e in enumerate(l):
+        divisors[(e, i)] = []
+
+    # Find divisors of each number
+    for i, e in enumerate(l):
+        for j, n in enumerate(l[i+1:]):
+            if n % e == 0:
+                divisors[(n, j+i+1)].append((e, i)) # (value, index)
+
+    # Calculate the number of triplets
+    triplets = 0
+    for num, div_num in divisors.items():
+        for d in div_num:
+            triplets += len(divisors[d])
+
+    return triplets
